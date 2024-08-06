5792c2718d1bce0743cecf1530c22f47c6cf43f7
URK23CS1219 <morensingh@karunya.edu.in>
2024-08-06 20:55:00 +0530

    program

--- lowercase_to_uppercase.java
+++ lowercase_to_uppercase.java
@@ -6,14 +6,13 @@ public class lowercase_to_uppercase {
         System.out.println("enter the string:");
         String x= sc.nextLine();
         char  y[]= x.toCharArray();
-        /* 
         for(int i=0;i<y.length;i++){
             if(y[i]>='a' && y[i]<='z'){
                 y[i]-=32;
             }
         }
-            */
-        //String s= new String(y);
+            
+        String s= new String(y);
         System.out.println("new string:"+y);
         sc.close();
     }
